6ad2b949bf69d66486474e0a09d1ca1bfd98804f
Jan De Poorter <jan@defv.be>
2015-02-03 13:38:35 +0100

    Add command names

--- lib/telos.rb
+++ lib/telos.rb
@@ -1,4 +1,30 @@
 module Telos
   autoload :Message, "telos/message.rb"
   autoload :Device, "telos/device.rb"
+
+  module Command
+    Login = 0
+    EnumerateShows = 1
+    EnumerateHostsForShow = 2
+    ConnectToShow = 4
+    ChangeShow = 3
+    EnumerateDirectors = 5
+    AttachToDirector = 6
+    AttachAsTalent = 7
+    PostBusyAllButton = 8
+    PostDelayDumpButton = 9
+    PostDialKey = 10
+    PostDropButton = 11
+    PostHoldButton = 12
+    PostLineButton = 13
+    PostMuteButton = 14
+    PostNextButton = 15
+    PostRecordButton = 16
+    PostSpeakerButton = 17
+    PostText = 18
+    PostTransferButton = 19
+    PostUpdateAll = 20
+    PostMuteRinger = 22
+    PostAcousticAddapt = 23
+  end
 end
